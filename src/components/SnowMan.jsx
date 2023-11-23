379dc8cc35297202f417e44c9333393a295e3a2b
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2023-11-23 16:04:06 +0100

    ajout apparition aléatoire des bonhommes de neiges

--- src/components/SnowMan.jsx
+++ src/components/SnowMan.jsx
@@ -1,21 +1,43 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import snowMenList from "./snowMenList";
 
 function SnowMan() {
   const [urlSnowMen, setUrlSnowMen] = useState("");
+  const [nameSnowMen, setNameSnowMen] = useState("");
+
+  const audio = useRef(null);
 
   useEffect(() => {
     const rand = Math.floor(Math.random() * 20);
+
     const apparitionSnowMen = setInterval(() => {
-      const indexSnowMenRandom = Math.floor(Math.random() * 5);
+      const indexSnowMenRandom = Math.floor(Math.random() * 7);
       setUrlSnowMen(snowMenList[indexSnowMenRandom].url);
+      setNameSnowMen(snowMenList[indexSnowMenRandom].name);
+      if (audio.current != null) {
+        audio.current.muted = false;
+        audio.current.play();
+      }
     }, rand * 1000);
     return () => clearInterval(apparitionSnowMen);
   }, []);
 
+  function handleDisapear() {
+    setUrlSnowMen("");
+  }
+
   return (
     <div className="snowMan">
-      <img className="snowmen" src={urlSnowMen} alt="snowMan" />
+      <audio ref={audio} muted={false}>
+        <track kind="captions" />
+        <source src="./sounds/laughtSnowMan.mp3" type="audio/mp3" />
+      </audio>
+      <img
+        className={nameSnowMen}
+        src={urlSnowMen}
+        onClick={handleDisapear}
+        alt="snowMan"
+      />
     </div>
   );
 }
--- src/components/snowMenList.js
+++ src/components/snowMenList.js
@@ -3,31 +3,36 @@ const snowMenList = [
         id: 1,
         name:'snowBoxer',
         url:'./images/snowManBoxer.png',
-        position:'',
     },
     {
         id:2,
         name:'snowFather',
         url:'./images/SnowManFather.png',
-        position:'',
     },
     {
         id:3,
         name:'snowHide',
         url:'./images/snowManHide.png',
-        position:'',
     },
     {
         id:4,
         name:'snowHide2',
         url:'./images/snowManHide2.png',
-        position:'',
     },
     {
         id:5,
         name:'snowSurfer',
         url:'./images/snowManSurfer.png',
-        position:'',
+    },
+    {
+        id:6,
+        name:'snowManHi',
+        url:'./images/snowManHi.png',
+    },
+    {
+        id:7,
+        name:'snowManHi2',
+        url:'./images/SnowManHi2.png',
     },
 ]
 
