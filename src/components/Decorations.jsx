00fa72ed90b06d721c6e5228a388774a5209dd15
Irwin <irwin.soliman@gmail.com>
2023-11-23 12:29:59 +0100

    decoration shop squelette

--- src/components/Decorations.jsx
+++ src/components/Decorations.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
 
 const Decorations = () => {
-    return (
-        <div>
-            deco
-        </div>
-    );
+  const table = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+  return (
+    <div className="decorations">
+      <div className="Filter">
+        <button>Boules</button>
+        <div>Etoiles</div>
+        <div>Extra</div>
+      </div>
+      {table.map((decoration) => {
+        return (
+          <div className="decoration" key={decoration}>
+            {decoration}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Decorations;
