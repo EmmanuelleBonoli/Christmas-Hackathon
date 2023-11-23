494c07059d6d3c3483451ee778ce1b408431a690
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2023-11-23 14:49:08 +0100

    apparition snowmen

--- src/components/SnowMan.jsx
+++ src/components/SnowMan.jsx
@@ -1,10 +1,23 @@
+import { useEffect, useState } from "react";
+import snowMenList from "./snowMenList";
 
-const SnowMan = () => {
-    return (
-        <div>
-            snowman
-        </div>
-    );
-};
+function SnowMan() {
+  const [urlSnowMen, setUrlSnowMen] = useState("");
+
+  useEffect(() => {
+    const rand = Math.floor(Math.random() * 20);
+    const apparitionSnowMen = setInterval(() => {
+      const indexSnowMenRandom = Math.floor(Math.random() * 5);
+      setUrlSnowMen(snowMenList[indexSnowMenRandom].url);
+    }, rand * 1000);
+    return () => clearInterval(apparitionSnowMen);
+  }, []);
+
+  return (
+    <div className="snowMan">
+      <img className="snowmen" src={urlSnowMen} alt="snowMan" />
+    </div>
+  );
+}
 
 export default SnowMan;
---
+++ src/components/snowMenList.js
@@ -0,0 +1,34 @@
+const snowMenList = [
+    {
+        id: 1,
+        name:'snowBoxer',
+        url:'./images/snowManBoxer.png',
+        position:'',
+    },
+    {
+        id:2,
+        name:'snowFather',
+        url:'./images/SnowManFather.png',
+        position:'',
+    },
+    {
+        id:3,
+        name:'snowHide',
+        url:'./images/snowManHide.png',
+        position:'',
+    },
+    {
+        id:4,
+        name:'snowHide2',
+        url:'./images/snowManHide2.png',
+        position:'',
+    },
+    {
+        id:5,
+        name:'snowSurfer',
+        url:'./images/snowManSurfer.png',
+        position:'',
+    },
+]
+
+export default snowMenList
