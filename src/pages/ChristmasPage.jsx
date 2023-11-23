01dcb016485f609221bc4c85f8d5bb2146e8af64
Aurelien <aurelien.emeriau@hotmail.fr>
2023-11-23 22:14:00 +0100

    commit modifs avant pull

--- src/pages/ChristmasPage.jsx
+++ src/pages/ChristmasPage.jsx
@@ -1,18 +1,58 @@
 import SnowMan from "../components/SnowMan";
+import React, { useState, useEffect, useRef } from "react";
 import Decorations from "../components/Decorations";
 import SnowEffect from "../components/SnowEffect";
 
 const ChristmasPage = () => {
+    const audioRef = useRef(null);
+  
+    const playAudio = () => {
+        if (audioRef.current) {
+          audioRef.current.play();
+        }
+      };
+
+    const CountdownTrain = () => {
+        const [count, setCount] = useState(); 
+      
+        useEffect(() => {
+          const countdownInterval = setInterval(() => {
+            if (count > 0) {
+              setCount(count - 1);
+            } else {
+              clearInterval(countdownInterval);
+            }
+          }, 9);
+      
+          return () => {
+            clearInterval(countdownInterval); // Nettoyer l'intervalle lors du démontage du composant
+          };
+        }, [count]);
+      
+        return (
+          <div>
+            <img className="traineau" src="./images/traineau.webp"></img>
+          </div>
+        );
+      };
+
     return (
-        <div className="christmasPage">
+        <div className="christmasPage" onClick={playAudio}>
             <Decorations />
             <SnowMan />
-            <SnowEffect />
+            <SnowEffect onClick={CountdownTrain}/>
             <img className="tree" src="./images/christmasTree.png"></img>
-            <img className="traineau" src="./images/traineau.webp"></img> 
+            <CountdownTrain />
+            <audio ref={audioRef}>
+            <source src="public/sounds/pereNoelSound.mp3" type="audio/mp3" />
+            <track kind="captions" />
+            </audio>
         </div>
+        
        
     );
 };
 
 export default ChristmasPage;
+
+
